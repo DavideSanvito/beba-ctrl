c19d0a1e3637bf8ea4de606f96ffbb75b742316a
Davide Sanvito <dsanvito90@gmail.com>
2016-10-13 17:03:28 -0700

    forwarding consistency test

--- ryu/app/beba/test/forwarding_consistency_1_to_many.py
+++ ryu/app/beba/test/forwarding_consistency_1_to_many.py
@@ -40,11 +40,16 @@ time.sleep(3)
 
 CONN_NUM = 20
 print 'Starting %d TCP connections from h1' %CONN_NUM
+
+net['h1'].cmd('(tcpdump -n -i any -l &> /tmp/tcpdumplog.h1) &')
+
 for n in range(CONN_NUM):
 	net['h1'].cmd('(echo "HI!" | nc -q -1 10.0.0.2 80) &')
 
 
 time.sleep(3)
+
+os.system('cat /tmp/tcpdumplog.h1')
 
 established = {}
 syn_recv = {}
